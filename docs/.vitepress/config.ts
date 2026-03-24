0c944ed5fd9f2f0407936d7b9b35d5c691c655e4
jingshui127 <jingshui127@163.com>
2026-03-24 12:55:30 +0800

    添加本地搜索功能

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,26 @@ export default withMermaid(
       outline: {
         level: [2, 4]
       },
+      search: {
+        provider: 'local',
+        options: {
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档'
+            },
+            modal: {
+              noResultsText: '无法找到相关结果',
+              resetButtonTitle: '清除查询条件',
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭'
+              }
+            }
+          }
+        }
+      },
       nav: [
         { text: '首页', link: '/' },
         { text: '功能文档', link: '/01_开合模功能整理' },
